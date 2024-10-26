06cfcbbe6e930d37e67e8ef9691bb3c6a73fe25d
SerenaCristian <devcristian02@gmail.com>
2024-10-26 18:53:20 -0300

    Montado as camadas

---
+++ CrudAvaliativoTechClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrudAvaliativoTechClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
