61fe6bf12ce88bf5b0ed2acb751eefe415b926ef
SerenaCristian <devcristian02@gmail.com>
2024-10-28 21:39:18 -0300

    Adicionando Metodos na classe Cliente repositório

--- CrudAvaliativoTechClass/Core/Entidades/Cliente.cs
+++ CrudAvaliativoTechClass/Core/Entidades/Cliente.cs
@@ -4,6 +4,7 @@ namespace CrudAvaliativoTechClass.Core.Entidades
 {
     internal class Cliente
     {
+        public int Id { get; set; }
         public string Nome { get; set; }
         public DateTime DataNascimento { get; set; }
         public string Email { get; set; }
--- CrudAvaliativoTechClass/Infra/Repositorios/ClienteRepositorio.cs
+++ CrudAvaliativoTechClass/Infra/Repositorios/ClienteRepositorio.cs
@@ -1,6 +1,56 @@
-﻿namespace CrudAvaliativoTechClass.Infra.Repositorios
+﻿using CrudAvaliativoTechClass.Core.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CrudAvaliativoTechClass.Infra.Repositorios
 {
     internal class ClienteRepositorio
     {
+        private static List<Cliente> clientes = new List<Cliente>();
+        private static int id = 0;
+
+        public void AdicionarCliente(Cliente cliente)
+        {
+            
+            var clienteExistente = clientes.FirstOrDefault(x => x.Email == cliente.Email);
+            if (clienteExistente != null)
+            {
+                Console.WriteLine("Usuário já cadastrado");
+                return;
+            }
+
+            cliente.Id = id++;
+            clientes.Add(cliente);
+        }
+
+        public void Atualizar(Cliente cliente)
+        {
+            var atualizarCliente = clientes.FirstOrDefault(x => x.Id == cliente.Id);
+
+            if (atualizarCliente != null)
+            {
+                atualizarCliente.Nome = cliente.Nome;
+                atualizarCliente.Email = cliente.Email;
+                atualizarCliente.Telefone = cliente.Telefone;
+                atualizarCliente.DataNascimento = cliente.DataNascimento;
+            }
+            else
+            {
+                Console.WriteLine("Cliente não encontrado para atualização.");
+            }
+        }
+
+        public void DeleteFisico(Cliente cliente)
+        {
+            clientes.Remove(cliente);
+        }
+
+        public List<Cliente> GetClientes()
+        {
+            return clientes;
+        }
     }
 }
+
+
